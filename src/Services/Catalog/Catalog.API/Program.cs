bb2bceb264115b983c15d35d92a7de97b8f88662
mohmedeprahem <m.eprahem.168@gmail.com>
2026-07-07 16:11:26 +0300

    feat: init Catalog microservice

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,17 @@
+using BuildingBlocks.Extentions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarterWithAssemblies(typeof(Program).Assembly);
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
