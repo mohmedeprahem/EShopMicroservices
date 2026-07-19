048a3b61bf008a06e6e36d188e7f0e514aa586a5
mohmedeprahem <m.eprahem.168@gmail.com>
2026-07-19 11:36:44 +0300

    feat: Add Get product By id api

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Pruduct);
+    internal class GetProductByIdHandler(IDocumentSession session, ILogger<GetProductByIdHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling GetProductByIdQuery for Id: {Id}", query);
+
+            var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
