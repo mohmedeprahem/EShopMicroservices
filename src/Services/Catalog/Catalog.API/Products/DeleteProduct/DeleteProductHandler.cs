8b74ed2556cd173f0092b9861fcc1aa41ff049e9
mohmedeprahem <m.eprahem.168@gmail.com>
2026-07-19 13:53:25 +0300

    refactor: Fix Delete product api

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    internal class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = await session.LoadAsync<Product>(command.id, cancellationToken);
+
+            if (product is null)
+                throw new ProductNotFoundException();
+
+            session.Delete(product);
+            await session.SaveChangesAsync();
+
+            return new DeleteProductResult(IsSuccess: true);
+        }
+    }
+}
