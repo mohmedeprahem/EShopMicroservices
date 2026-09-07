c5ff26d9656d07080d267233364e4ca416674f6b
mohmedeprahem <m.eprahem.168@gmail.com>
2026-09-07 15:39:11 +0300

    feat: Implement service of Discount

---
+++ src/Services/Discount/Discount.Grpc/Data/Extension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class MigrationExtensions
+    {
+        public static async Task ApplyMigrationsAsync(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            await context.Database.MigrateAsync();
+        }
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,15 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpcReflection();
+
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")!);
+});
 
 // Add services to the container.
 builder.Services.AddGrpc();
@@ -8,12 +17,14 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapGrpcReflectionService();
 }
+
+await app.ApplyMigrationsAsync();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
