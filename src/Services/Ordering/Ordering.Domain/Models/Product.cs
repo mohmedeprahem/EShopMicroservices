a1a69298edc7a395b1b538a8176873e32ed49b69
mohmedeprahem <m.eprahem.168@gmail.com>
2026-09-14 15:39:00 +0300

    feat: Add events and Order and order item entites in Order micro

---
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Domain.Models;
+public sealed class Product : Entity<ProductId>
+{
+    public string Name { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+
+    private Product(ProductId id, string name, decimal price) : base(id)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public static Product Create(ProductId id, string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        return new Product(id, name, price);
+    }
+}
